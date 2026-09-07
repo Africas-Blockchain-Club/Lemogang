d05c6b77d6826a9b1acff1bfd3c5121b584373e2
Lemogang <lentlswgc025@student.wethinkcode.co.za>
2026-09-08 01:03:10 +0200

    Add admincontrol contract and an incomplete contract to link both schools

--- SolidityBasics/school.sol
+++ SolidityBasics/school.sol
@@ -1,6 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+
+// A contract setup to manage student information between two different schools by one central admin.
+
 contract school {
     string public schoolName;
 
@@ -16,7 +19,7 @@ contract school {
 contract WTC is school {
      struct Student {
         string name;
-        uint Gradelevel;
+        uint standard;
     }
 
     mapping(uint => Student) public students;
@@ -25,13 +28,13 @@ contract WTC is school {
         
     }
 
-    function addStudent(uint _id, string calldata _name, uint _Gradelevel) public {
-        students[_id] = Student(_name, _Gradelevel );
+    function addStudent(uint _id, string calldata _name, uint _standard) public {
+        students[_id] = Student(_name, _standard );
     }
 
     function getStudent(uint _id) public view returns (string memory, uint ) {
         Student memory s = students[_id];
-        return (s.name, s.Gradelevel );
+        return (s.name, s.standard );
     }
 }
 
@@ -56,6 +59,44 @@ contract ABC is school {
         Student memory s = students[_id];
         return (s.name, s.level);
     }
+}
+
+contract AdminControl {
+    address public admin;
+
+    constructor() {
+        admin = msg.sender;
+    }
+
+    modifier onlyAdmin() {
+        require(msg.sender == admin, "Not admin");
+        _;
+    }
+
+    function changeAdmin(address _newAdmin) public onlyAdmin {
+        admin = _newAdmin;
+    }
+}
+
+// link both schools together where the admin can add students to either school
+
+contract is ABC,WTC,AdminControl{
+
+    constructor(string _schoolName)
+        ABC(_schoolName)
+        WTC(_schoolName)
+        AdminControl(){
+    }
+
+
+    function getFullABCDetails(uint _id) public view returns(string memory name, string memory standard, string memory school, address admin) {
+        return getABCDetails(_id);
+    }
+
+    function getFullWTCDetails(uint _id) public view returns(string memory name, uint256 level, string memory school, address admin) {
+        return getWTCDetails(_id);
+    }
+
 }
 
 
